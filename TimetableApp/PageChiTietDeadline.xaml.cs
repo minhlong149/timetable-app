defc0f787d23020e172a2eb201016d79b6ba563d
Norman-Tran <113096397+Norman-Tran@users.noreply.github.com>
2022-12-08 07:46:45 +0700

    Update PageDeadline (still have errors)

---
+++ TimetableApp/PageChiTietDeadline.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimetableApp.Class;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimetableApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageChiTietDeadline : ContentPage
+    {
+        public PageChiTietDeadline(Deadline deadline)
+        {
+
+            InitializeComponent();
+        }
+
+        private void Add_Invoked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
