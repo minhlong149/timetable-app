3a36f6d09180a96ff7d782541945fcdbf3854ea8
Norman-Tran <113096397+Norman-Tran@users.noreply.github.com>
2022-12-26 15:46:10 +0700

    Update function notification and deadline Update MainPage.xaml

--- TimetableApp/AdminViews/PageTaoThongBao.xaml.cs
+++ TimetableApp/AdminViews/PageTaoThongBao.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using TimetableApp.Class;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,36 @@ namespace TimetableApp.AdminViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PageTaoThongBao : ContentPage
     {
+        ThongBao _ThongBao;
         public PageTaoThongBao()
         {
             InitializeComponent();
+            AddMaSV.Focus();
+            AddNoiDung.Focus();
+        }
+
+        private async void Save_Clicked(object sender, EventArgs e)
+        {
+            ThongBao _ThongBao = new ThongBao();
+            _ThongBao.ID = 1;
+            _ThongBao.MaSV = AddMaSV.Text;
+            _ThongBao.NoiDung= AddNoiDung.Text;
+
+            HttpClient httpClient = new HttpClient();
+            string json = JsonConvert.SerializeObject(_ThongBao);
+            StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage kq;
+
+            kq = await httpClient.PostAsync("http://www.lno-ie307.somee.com/api/Notification?NoiDung=" + AddNoiDung.Text, stringContent);
+            var kqthem = await kq.Content.ReadAsStringAsync();
+            if (int.Parse(kqthem.ToString()) > 0)
+            {
+                await DisplayAlert("Thông báo", "Thêm thông báo thành công", "OK");
+            }
+            else
+                await DisplayAlert("Thông báo", "Thêm thông báo không thành công", "Thử lại");
+
+            await Navigation.PopAsync();
         }
     }
 }
--- TimetableApp/PageDeadline.xaml.cs
+++ TimetableApp/PageDeadline.xaml.cs
@@ -20,6 +20,7 @@ namespace TimetableApp
             InitializeComponent();
             GetDeadlineByMaSVAndMaLop();
         }
+
         async void GetDeadlineByMaSVAndMaLop()
         {
             HttpClient httpClient = new HttpClient();
@@ -30,8 +31,41 @@ namespace TimetableApp
 
         private void LstDeadline_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            Deadline deadline= (Deadline)e.SelectedItem;
+            Deadline deadline = (Deadline)e.SelectedItem;
             Navigation.PushModalAsync(new PageChiTietDeadline(deadline));
+        }
+
+        private void TIAddDeadline_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageAdDeadline());
+        }
+
+        private async void DelDeadline_Invoked(object sender, EventArgs e)
+        {
+            SwipeItem swipeItem = (SwipeItem)sender;
+            Deadline deadline = swipeItem.CommandParameter as Deadline;
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage kq;
+            kq = await httpClient.DeleteAsync("http://www.lno-ie307.somee.com/api/Homework?ID=" + deadline.ID.ToString());
+            var kqdel = await kq.Content.ReadAsStringAsync();
+            if (int.Parse(kqdel.ToString()) > 0)
+                await DisplayAlert("Thông báo", "Đã xóa deadline thành công!", "OK");
+            else
+                await DisplayAlert("Thông báo", "Không thể xóa!\tVui lòng thử lại", "OK");
+        }
+
+        private void UpDateDeadline_Invoked(object sender, EventArgs e)
+        {
+            SwipeItem swipeItem = (SwipeItem)sender;
+            Deadline deadline = swipeItem.CommandParameter as Deadline;
+            Navigation.PushAsync(new PageAdDeadline(deadline));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetDeadlineByMaSVAndMaLop();
         }
     }
 }
