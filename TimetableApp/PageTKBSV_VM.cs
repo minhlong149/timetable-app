623edb8e67fe8b42f2613f4a8970d8bad4c6315b
minhlong149 <minhlong9514@gmail.com>
2023-01-05 11:23:18 +0700

    Demo calendar

---
+++ TimetableApp/PageTKBSV_VM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimetableApp.Class;
+using Xamarin.Plugin.Calendar.Models;
+
+namespace TimetableApp
+{
+    internal class PageTKBSV_VM
+    {
+        public EventCollection Events { get; set; }
+
+        public PageTKBSV_VM()
+        {
+            Events = new EventCollection
+            {
+                //[DateTime.Now] = new List<LopHoc>
+                //{
+                //    new LopHoc { Name = "Cool event1", Description = "This is Cool event1's description!" },
+                //    new LopHoc { Name = "Cool event2", Description = "This is Cool event2's description!" }
+                //},
+            };
+        }
+    }
+}
